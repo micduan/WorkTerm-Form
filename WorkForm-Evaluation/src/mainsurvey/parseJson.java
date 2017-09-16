b6ff5a6997e7af9e9802bc2af9feb92642f14ac1
micduan <michael.duan123@gmail.com>
2017-09-16 17:25:32 -0400

    returns properly

--- WorkForm-Evaluation/src/mainsurvey/parseJson.java
+++ WorkForm-Evaluation/src/mainsurvey/parseJson.java
@@ -1,28 +1,22 @@
 package mainsurvey;
 
-import java.io.File;
-import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
-
-import net.sf.json.JSONSerializer; 
-
-import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
-
 public class parseJson {
+	static StringBuilder sb;
+	private static String jsonString;
 
     public static void main(String[] args) throws Exception {
     	readJson();
     }
     
-    public static JSONObject readJson() throws IOException {
+    public static String readJson() throws IOException {
     	BufferedReader br = null;
     	String line;
-    	StringBuilder sb = new StringBuilder();
+    	sb = new StringBuilder();
     	
     	try {
     		br = new BufferedReader(new FileReader("/Users/micduan/Documents/Side projects/WorkTerm-Evaluation/WorkForm-Evaluation/src/mainsurvey/questions.json"));
@@ -35,14 +29,24 @@ public class parseJson {
     	    while ((line = br.readLine()) != null) {
     	        sb.append(line);
     	    }
-    	    System.out.println(sb);
-    	    JSONObject json = new JSONObject(sb.toString());
-    	    return json;
+    	    jsonString = sb.toString();
+    	    return jsonString;
     	} catch (IOException e) {
     		e.printStackTrace();
     	} finally {
     		br.close();
     	}
-		return null;
+    	return jsonString;
+    }
+    
+    public static String getQuestions() {
+    	//return jsonString;
+    	try {
+			return readJson();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "Error";
+		}
     }
 }
