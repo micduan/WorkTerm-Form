7ccc25d8d895c78ca635a54474e2e0434f6592fc
micduan <michael.duan123@gmail.com>
2017-09-16 16:31:13 -0400

    added json parsing from local file

--- WorkForm-Evaluation/src/mainsurvey/parseJson.java
+++ WorkForm-Evaluation/src/mainsurvey/parseJson.java
@@ -1,20 +1,48 @@
 package mainsurvey;
 
+import java.io.File;
 import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 
-import net.sf.json.JSONObject; 
 import net.sf.json.JSONSerializer; 
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
 
 
 public class parseJson {
 
     public static void main(String[] args) throws Exception {
-        InputStream rawTxt = parseJson.class.getResourceAsStream( "examplejson.txt");
-        String jsonTxt = IOUtils.toString( rawTxt );
+    	readJson();
+    }
+    
+    public static JSONObject readJson() throws IOException {
+    	BufferedReader br = null;
+    	String line;
+    	StringBuilder sb = new StringBuilder();
+    	
+    	try {
+    		br = new BufferedReader(new FileReader("/Users/micduan/Documents/Side projects/WorkTerm-Evaluation/WorkForm-Evaluation/src/mainsurvey/questions.json"));
 
-        JSONObject json = (JSONObject) JSONSerializer.toJSON( jsonTxt ); 
-        System.out.println(json);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	try {
+    	    while ((line = br.readLine()) != null) {
+    	        sb.append(line);
+    	    }
+    	    System.out.println(sb);
+    	    JSONObject json = new JSONObject(sb.toString());
+    	    return json;
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	} finally {
+    		br.close();
+    	}
+		return null;
     }
 }
